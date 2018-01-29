6cacabb85a3374647671d3cb5287a238a40d80ba
enricovianello <maverick17586@gmail.com>
2018-01-29 10:13:20 +0100

    Refixed MODE environment var

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -53,9 +53,10 @@ function cleanup(){
 
 trap cleanup EXIT SIGINT SIGTERM SIGABRT
 
+export MODE=${MODE}
 cd docker
 docker network create cnaf.infn.it
-docker-compose up -e MODE=${MODE} --abort-on-container-exit 
+docker-compose up --abort-on-container-exit 
 
 set +e
 
